5a55ed3c1e51c1908150ba736f7a735ff2d919c6
sven <42868792+FreerGit@users.noreply.github.com>
2025-08-02 18:12:09 +0200

    polymorphism

--- exercises/polymorphism/trypoly.cpp
+++ exercises/polymorphism/trypoly.cpp
@@ -1,16 +1,19 @@
 #include "Polygons.hpp"
 #include <iostream>
 
-int main() {
+int main()
+{
     // create a Pentagon, call its perimeter method
-
-
+    auto p = Pentagon(5.0);
+    p.computePerimeter();
     // create a Hexagon, call its perimeter method
-
-
+    auto h = Hexagon(4.0);
+    h.computePerimeter();
     // create a Hexagon, call the perimeter method through a reference to Polygon
-
+    auto hexa2 = Hexagon(1.0);
+    RegularPolygon& poly = hexa2;
+    hexa2.computePerimeter();
+    poly.computePerimeter();
 
     // retry virtual method
-
 }
