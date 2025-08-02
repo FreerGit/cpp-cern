e2cca32e0b9806df71be37e854e336915da3ef1e
sven <42868792+FreerGit@users.noreply.github.com>
2025-08-02 19:41:51 +0200

    virtual inheritance

--- exercises/virtual_inheritance/trymultiherit.cpp
+++ exercises/virtual_inheritance/trymultiherit.cpp
@@ -3,12 +3,10 @@
 
 int main() {
     // create a TextBox and call draw
-
+    TextBox tb = TextBox("content", 1080, 1080);
 
     // Fix the code to call both draws by using types
-
-
+    static_cast<Text>(tb).draw();
+    static_cast<Rectangle>(tb).draw();
     // try with virtual inheritance
-
-
 }
