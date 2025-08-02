7ae0fc0c0c13c4b9994211a2b9a94cf6e023c84a
sven <42868792+FreerGit@users.noreply.github.com>
2025-08-02 17:55:01 +0200

    functions and loopsrefsauto

--- exercises/control/control.cpp
+++ exercises/control/control.cpp
@@ -1,54 +1,64 @@
+#include <algorithm>
 #include <iostream>
 
-const unsigned int numbers[]{1000001, 1000002, 1000003, 1000004, 1000005, 1000006, 1000007, 1000008, 1000009};
+const unsigned int numbers[] { 1000001, 1000002, 1000003, 1000004, 1000005, 1000006, 1000007, 1000008, 1000009 };
 
 bool isodd(unsigned int i) { return i % 2 == 1; }
 
-void part1() {
-  unsigned int sum_odd = 0;
-  unsigned int sum_eve = 0;
-  for (int i = 0; i < 9; ++i) {
-    unsigned int num = numbers[i];
-    if (isodd(num)) {
-      sum_odd += num;
-    } else {
-      sum_eve += num;
+void part1()
+{
+    unsigned int sum_odd = 0;
+    unsigned int sum_eve = 0;
+    for (unsigned int num : numbers) {
+        (isodd(num) ? sum_odd : sum_eve) += num;
     }
-  }
-  std::cout << "Sums: odd = " << sum_odd << ", even = " << sum_eve << "\n";
+    std::cout << "Sums: odd = " << sum_odd << ", even = " << sum_eve << "\n";
 }
 
-void part2() {
-  // print smallest n for which 1 + 2 + ... + n > 10000
-  int sum = 0;
-  for (int i = 1; ; i++) {
-    sum += i;
-    if (sum > 10000) {
-      std::cout << i << "\n";
-      break;
+void part2()
+{
+    // print smallest n for which 1 + 2 + ... + n > 10000
+    int sum = 0;
+    int acc = 1;
+
+    do {
+        acc += 1;
+        sum += acc;
+    } while (sum <= 10000);
+    std::cout << acc << "\n";
+}
+
+enum class Language { English,
+    French,
+    German,
+    Italian,
+    Other };
+
+void part3(Language l)
+{
+    switch (l) {
+    case Language::English:
+        std::cout << "Hello" << std::endl;
+        break;
+    case Language::French:
+        std::cout << "Salut" << std::endl;
+        break;
+    case Language::German:
+        std::cout << "Hallo" << std::endl;
+        break;
+    case Language::Italian:
+        std::cout << "Ciao" << std::endl;
+        break;
+    case Language::Other:
+        std::cout << "I don't speak your language" << std::endl;
+        break;
     }
-  }
 }
 
-enum class Language { English, French, German, Italian, Other };
-
-void part3(Language l) {
-  if (l == Language::English) {
-    std::cout << "Hello\n";
-  } else if (l == Language::French) {
-    std::cout << "Salut\n";
-  } else if (l == Language::German) {
-    std::cout << "Hallo\n";
-  } else if (l == Language::Italian) {
-    std::cout << "Ciao\n";
-  } else {
-    std::cout << "I don't speak your language\n";
-  }
-}
-
-int main() {
-  part1();
-  part2();
-  part3(Language::English);
-  return 0;
+int main()
+{
+    part1();
+    part2();
+    part3(Language::English);
+    return 0;
 }
