0ef36944bdaeeca250815d27e484d07c60e65e72
sven <42868792+FreerGit@users.noreply.github.com>
2025-08-10 22:37:27 +0200

    cond var

--- exercises/condition_variable/condition_variable.cpp
+++ exercises/condition_variable/condition_variable.cpp
@@ -1,5 +1,5 @@
-#include <condition_variable>
 #include <chrono>
+#include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <sstream>
@@ -17,53 +17,51 @@ using namespace std::chrono_literals; // We can write 1s
 // This class consumes stream inputs, buffers them, and writes them
 // out when destructed respecting the cout_mutex.
 class SafeCout {
-  std::stringstream stream;
-  inline static std::mutex cout_mutex; // We need this to synchronise printing
+    std::stringstream stream;
+    inline static std::mutex cout_mutex; // We need this to synchronise printing
 
-public:
-  ~SafeCout() {
-    std::scoped_lock<std::mutex> coutLock{cout_mutex};
-    std::cout << stream.str();
-  }
+  public:
+    ~SafeCout() {
+        std::scoped_lock<std::mutex> coutLock{cout_mutex};
+        std::cout << stream.str();
+    }
 
-  template<typename T>
-  SafeCout & operator<<(T&& arg) {
-    stream << std::forward<T>(arg);
-    return *this;
-  }
+    template <typename T> SafeCout &operator<<(T &&arg) {
+        stream << std::forward<T>(arg);
+        return *this;
+    }
 };
 
 // A mock data object
 struct Data {
-  bool isReady() const {
-    return _isReady;
-  }
+    bool isReady() const {
+        return _isReady;
+    }
 
-  bool _isReady = false;
-  bool _isConsistent = false;
+    bool _isReady = false;
+    bool _isConsistent = false;
 };
-
 
 // The function that processes the data. You don't need to touch it.
 // It will check whether the data are in a consistent state, and
 // idle a bit to simulate longer data processing.
 // Note: It should run in parallel.
-bool process(unsigned int threadIdx, Data const & data) {
-  bool processingOK = true;
+bool process(unsigned int threadIdx, Data const &data) {
+    bool processingOK = true;
 
-  SafeCout{} << '[' << threadIdx << "] I'm starting to process the data now\n";
-  if (!data._isConsistent) {
-    processingOK = false;
-    SafeCout{} << '[' << threadIdx << "] ERROR data isn't fully ready! Race condition!\n";
-  }
+    SafeCout{} << '[' << threadIdx << "] I'm starting to process the data now\n";
+    if (!data._isConsistent) {
+        processingOK = false;
+        SafeCout{} << '[' << threadIdx << "] ERROR data isn't fully ready! Race condition!\n";
+    }
 
-  // Burn some CPU cycles to simulate intensive data processing.
-  const auto startTime = std::chrono::high_resolution_clock::now();
-  unsigned dummyCounter = 0;
-  while (std::chrono::high_resolution_clock::now() - startTime < 5s) {
-    ++dummyCounter;
-  }
-  return processingOK;
+    // Burn some CPU cycles to simulate intensive data processing.
+    const auto startTime = std::chrono::high_resolution_clock::now();
+    unsigned dummyCounter = 0;
+    while (std::chrono::high_resolution_clock::now() - startTime < 5s) {
+        ++dummyCounter;
+    }
+    return processingOK;
 }
 
 /*
@@ -81,60 +79,61 @@ bool process(unsigned int threadIdx, Data const & data) {
  *    Run it multiple times if necessary.
  *    Why do they wake up although they should remain sleeping?
  * 2. Fix the race condition by protecting the data production phase with a lock.
- * 3. When you run the program now, the consumers should start running only after the data are ready.
- *    Why do they run one by one, though?
- *    Check the CPU consumption with a tool like top in a second shell.
- *    Ideally, we want 400% utilisation.
+ * 3. When you run the program now, the consumers should start running only after the data are
+ * ready. Why do they run one by one, though? Check the CPU consumption with a tool like top in a
+ * second shell. Ideally, we want 400% utilisation.
  * 4. Fix the consumer waiting phase like in the lecture to make the consumers run in parallel.
  *    Check the CPU consumption again.
  */
 
-
 int main() {
-  std::mutex mutex;
-  std::condition_variable cond;
-  Data data;
+    std::mutex mutex;
+    std::condition_variable cond;
+    Data data;
 
-  // DATA-PROCESSING THREADS
-  // Here we start the processing threads. They have to wait for the data to be ready,
-  // and then they should process it in parallel.
-  auto processData = [&](unsigned int threadIdx){
-    SafeCout{} << '[' << threadIdx << "] I'm starting to wait\n";
+    // DATA-PROCESSING THREADS
+    // Here we start the processing threads. They have to wait for the data to be ready,
+    // and then they should process it in parallel.
+    auto processData = [&](unsigned int threadIdx) {
+        SafeCout{} << '[' << threadIdx << "] I'm starting to wait\n";
+        {
+            std::unique_lock<std::mutex> lock{mutex};
+            cond.wait(lock, [&]() { return data.isReady(); });
+        }
+        auto result = process(threadIdx, data);
 
-    std::unique_lock<std::mutex> lock{mutex};
-    cond.wait(lock, [&](){ return data.isReady(); });
+        SafeCout{} << '[' << threadIdx << "] Data processing completed "
+                   << (result ? "OK" : "with failure!") << '\n';
+    };
 
-    auto result = process(threadIdx, data);
+    std::vector<std::thread> consumers;
+    for (unsigned int i = 0; i < 4; ++i) {
+        consumers.emplace_back(processData, i);
+    }
 
-    SafeCout{} << '[' << threadIdx << "] Data processing completed " << (result ? "OK" : "with failure!") << '\n';
-  };
+    // DATA-PRODUCER THREAD
+    // This thread produces the data. We simulate a complicated way of producing the data
+    // by making the thread wait for a few seconds during the data production.
+    std::thread producer([&]() {
+        SafeCout{} << "[p] Starting to produce data\n";
+        {
+            std::lock_guard lock(mutex);
+            data._isReady = true;
+            // Sleep a bit to simulate a complicated set up phase
+            std::this_thread::sleep_for(6s);
+            data._isConsistent = true;
+        }
+        SafeCout{} << "[p] Data ready now\n";
 
-  std::vector<std::thread> consumers;
-  for (unsigned int i=0; i < 4; ++i) {
-    consumers.emplace_back(processData, i);
-  }
+        // Wake up all threads
+        cond.notify_all();
+    });
 
-  // DATA-PRODUCER THREAD
-  // This thread produces the data. We simulate a complicated way of producing the data
-  // by making the thread wait for a few seconds during the data production.
-  std::thread producer([&](){
-    SafeCout{} << "[p] Starting to produce data\n";
-    data._isReady = true;
-    // Sleep a bit to simulate a complicated set up phase
-    std::this_thread::sleep_for(6s);
-    data._isConsistent = true;
+    // Join all threads, so we don't terminate prematurely
+    producer.join();
+    for (auto &t : consumers) {
+        t.join();
+    }
 
-    SafeCout{} << "[p] Data ready now\n";
-
-    // Wake up all threads
-    cond.notify_all();
-  });
-
-  // Join all threads, so we don't terminate prematurely
-  producer.join();
-  for (auto & t : consumers) {
-    t.join();
-  }
-
-  return 0;
+    return 0;
 }
