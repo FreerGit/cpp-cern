877d7d72d216f489aacdc736da1f5d21e642a4d2
sven <42868792+FreerGit@users.noreply.github.com>
2025-08-10 22:29:47 +0200

    race

--- exercises/race/racing.cpp
+++ exercises/race/racing.cpp
@@ -1,5 +1,7 @@
 
+#include <atomic>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -16,29 +18,31 @@ constexpr std::size_t nInc = 1000;
 constexpr std::size_t nRepeat = 1000;
 
 int main() {
-  int nError = 0;
+    int nError = 0;
 
-  for (std::size_t j = 0; j < nRepeat; j++) {
-    int a = 0;
+    for (std::size_t j = 0; j < nRepeat; j++) {
+        std::atomic<int> a{0};
+        // Increment the variable a 100 times:
+        // lambda inc
+        auto increment = [&a]() {
+            for (std::size_t i = 0; i < nInc; ++i) {
+                a++;
+            }
+        };
 
-    // Increment the variable a 100 times:
-    auto increment = [&a](){
-      for (std::size_t i = 0; i < nInc; ++i) {
-        a++;
-      }
-    };
+        // Start up all threads
+        std::vector<std::thread> threads;
+        for (std::size_t i = 0; i < nThread; ++i)
+            threads.emplace_back(increment);
+        for (auto &thread : threads)
+            thread.join();
 
-    // Start up all threads
-    std::vector<std::thread> threads;
-    for (std::size_t i = 0; i < nThread; ++i) threads.emplace_back(increment);
-    for (auto & thread : threads) thread.join();
-
-    // Check
-    if (a != nThread * nInc) {
-      std::cerr << "Race detected! Result: " << a << '\n';
-      nError++;
+        // Check
+        if (a != nThread * nInc) {
+            std::cerr << "Race detected! Result: " << a << '\n';
+            nError++;
+        }
     }
-  }
 
-  return nError;
+    return nError;
 }
